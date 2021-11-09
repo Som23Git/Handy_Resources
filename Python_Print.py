a359a86ea856850aa199daabbc97cb9dff90e1ef
Someshwaran Mohankumar <someshwaran.mohankumar@Someshwarans-MacBook-Pro.local>
2021-11-09 21:29:30 +0530

    Added Python print

---
+++ Python_Print.py
@@ -0,0 +1,19 @@
+print("Somesh"+"waran"+" M")
+print("""Line 2
+where Someshwaran
+M""") # This piece of print will print the code in 3 lines as given in three lines and also, """ three quotes will automatically open a newline
+print('\n''\n'"Line 3 with escape newline"'\n''\n')
+print('Single Quote String')
+print(42) #Numbers or integers do not need single or double quotes(not mandatory)
+print(3*"7")
+print(3**7)
+print(3+6)
+x=7 #declaring variables
+y=29
+print(x**y) #print statement
+for i in range(6):
+   for j in range(0,i):
+     print(i, end="")
+   print('\n')
+
+
